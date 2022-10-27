a31e7c49f12fcecb8e4adcad81fbb026adab8129
david <d.zanella@campus.fct.unl.pt>
2022-10-27 23:28:50 +0100

    problem solved

--- cgi-bin/removeCourse.py
+++ cgi-bin/removeCourse.py
@@ -15,31 +15,23 @@ db_connection = sqlite3.connect('curricularUnits.db')
 cursor = db_connection.cursor()
 cursor.execute("SELECT COUNT(*) FROM Courses WHERE course_id = ?;", [course_id])
 nC = cursor.fetchone()
-if(nC==0):
+if(nC[0] > 0):
+    cursor.execute('DELETE FROM Courses WHERE course_id = ?', [course_id])
     print("Content-type:text/html\n\n \
         <!DOCTYPE html> \
         <head> \
-            <h3> Course " + course_id + " does not exist </h3> \
+            <h3> Course " + course_id + " was removed</h3> \
+        </head> \
+        <body> ")
+else:
+    print("Content-type:text/html\n\n \
+        <!DOCTYPE html> \
+        <head> \
+            <h3> Course " + course_id + " doesn't exist </h3> \
         </head> \
         body> ")
-else: 
-    try:
-        cursor.execute('DELETE FROM Courses WHERE course_id = ?', [course_id])
-        print("Content-type:text/html\n\n \
-            <!DOCTYPE html> \
-            <head> \
-                <h3> Course " + course_id + " was removed </h3> \
-            </head> \
-            <body> ")
-    except sqlite3.Error as er:
-	    print("Content-type:text/html\n\n \
-            <!DOCTYPE html> \
-            <head> \
-                <h3> Course " + course_id + " does not exist </h3> \
-            </head> \
-            body> ")
-
-
+    
+    
     
  
 db_connection.commit()
