d4dcda6fdfa4b61cc1b7b8b2d281454593637d14
david <d.zanella@campus.fct.unl.pt>
2022-10-28 12:23:28 +0100

    to be solved

--- cgi-bin/changeStudentNumber.py
+++ cgi-bin/changeStudentNumber.py
@@ -6,11 +6,26 @@ import sys
 import codecs
 
 sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+print("Content-type:text/html\n\n \
+            <!DOCTYPE html> \
+            <head> \
+                <title>Change</title>\
+            </head> \
+            <body> ")
     
 form = cgi.FieldStorage()
 
 course_id = str(form["course_id"].value)
 students_enrolled = str(form["students_enrolled"].value)
+
+try:
+    int(students_enrolled)
+except Exception as e:
+    print('<h2> Input [Students enrolled] error, should be an integer </h2> <p>')
+   print("""  <p> <a href="../index.html" > Return to main page </a> </p>
+         </body>
+         </html>""")
+
 
 db_connection = sqlite3.connect('curricularUnits.db')
 cursor = db_connection.cursor()
@@ -20,26 +35,11 @@ try:
     n = cursor.fetchone()   # Check if user_id exists
     if (n[0] > 0):
         cursor.execute('UPDATE Courses SET students_enrolled = (?) WHERE course_id = (?)', (int(students_enrolled), int(course_id)))
-        print("Content-type:text/html\n\n \
-            <!DOCTYPE html> \
-            <head> \
-                <h3> Course " + course_id + " number of students enrolled was set to " + students_enrolled + " </h3> \
-            </head> \
-            <body> ")
+        print("<h3> Course " + course_id + " number of students enrolled was set to " + students_enrolled + " </h3>")
     else:
-        print("Content-type:text/html\n\n \
-            <!DOCTYPE html> \
-            <head> \
-                <h3> Course " + course_id + " does not exist </h3> \
-            </head> \
-            <body> ")
+        print("<h3> Course " + course_id + " does not exist </h3>")
 except sqlite3.Error as er:
-	print("Content-type:text/html\n\n \
-        <!DOCTYPE html> \
-        <head> \
-            <h3> UNEXPECTED ERROR " + str(er) + "</h3> \
-        </head> \
-        <body> ")
+	print("<h3> UNEXPECTED ERROR " + str(er) + "</h3>")
 db_connection.commit()
 db_connection.close()
 
